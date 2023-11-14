cee66039093bdcb34d8cb41c799b8c390c440119
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-11-14 15:05:26 -0800

    feat: complete desktop version of website

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
-const onePlayerGame = () => {
-	let userChoice = '';
-	let compChoice = '';
+let userChoice = '';
+let compChoice = '';
+let userScore = 0;
+let compScore = 0;
+
+const userScoreOutput = document.getElementById('score-user');
+const compScoreOutput = document.getElementById('score-comp');
+const decisionDisplay = document.getElementById('decision');
+const winLossDisplay = document.getElementById('win-or-loss');
+
+userScoreOutput.innerText = userScore;
+compScoreOutput.innerText = compScore;
+
+const weapons = ['Rock', 'Paper', 'Scissors'];
+
+const playerChoice = (choice) => {
+	userChoice = choice;
+	compChoice = weapons[Math.floor(Math.random() * 3)];
+
+	if (userChoice === 'Rock' && compChoice === 'Scissors') {
+		decisionDisplay.innerText = `${userChoice} beats ${compChoice}`;
+		winLossDisplay.innerText = 'You Win!';
+		userScoreOutput.innerText = ++userScore;
+	} else if (userChoice === 'Paper' && compChoice === 'Rock') {
+		decisionDisplay.innerText = `${userChoice} beats ${compChoice}`;
+		winLossDisplay.innerText = 'You Win!';
+		userScoreOutput.innerText = ++userScore;
+	} else if (userChoice === 'Scissors' && compChoice === 'Paper') {
+		decisionDisplay.innerText = `${userChoice} beats ${compChoice}`;
+		winLossDisplay.innerText = 'You Win!';
+		userScoreOutput.innerText = ++userScore;
+	} else if (userChoice === 'Rock' && compChoice === 'Paper') {
+		decisionDisplay.innerText = `${userChoice} beats ${compChoice}`;
+		winLossDisplay.innerText = 'You Lose';
+		compScoreOutput.innerText = ++compScore;
+	} else if (userChoice === 'Paper' && compChoice === 'Scissors') {
+		decisionDisplay.innerText = `${userChoice} beats ${compChoice}`;
+		winLossDisplay.innerText = 'You Lose';
+		compScoreOutput.innerText = ++compScore;
+	} else if (userChoice === 'Scissors' && compChoice === 'Rock') {
+		decisionDisplay.innerText = `${userChoice} beats ${compChoice}`;
+		winLossDisplay.innerText = 'You Lose';
+		compScoreOutput.innerText = ++compScore;
+	} else if (userChoice === compChoice) {
+		decisionDisplay.innerText = `${userChoice} equals ${compChoice}`;
+		winLossDisplay.innerText = "It's a Tie";
+	}
 };
